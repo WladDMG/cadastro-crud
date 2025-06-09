5f31c53f5ebf4062c49075165f676bcba1a64531
wladmir <wladdmg@gmail.com>
2025-06-09 03:24:47 -0300

    rev

--- cadastro-crud-api/src/auth/local.strategy.ts
+++ cadastro-crud-api/src/auth/local.strategy.ts
@@ -1,24 +1,3 @@
-// import { Strategy } from 'passport-local';
-// import { PassportStrategy } from '@nestjs/passport';
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-// import { AuthService } from './auth.service';
-
-// @Injectable()
-// export class LocalStrategy extends PassportStrategy(Strategy) {
-//   constructor(private readonly authService: AuthService) {
-//     super({ usernameField: 'email' }); // usar email no lugar de username
-//   }
-
-//   // o método validate do Passport
-//   async validate(email: string, password: string): Promise<any> {
-//     // chama AuthService.validate, que agora existe
-//     const user = await this.authService.validate(email, password);
-//     if (!user) {
-//       throw new UnauthorizedException();
-//     }
-//     return user;
-//   }
-// }
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
@@ -30,7 +9,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super({ usernameField: 'email' });
   }
 
-  // aqui usamos o validate que acabamos de criar
+
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validate(email, password);
     if (!user) {
--- cadastro-crud-api/src/main.ts
+++ cadastro-crud-api/src/main.ts
@@ -1,18 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Permite CORS para apps externos
   app.enableCors();
-
   await app.listen(3000);
 }
 bootstrap();
